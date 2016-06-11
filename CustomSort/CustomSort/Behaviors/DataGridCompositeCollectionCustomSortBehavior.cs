0ebc632c025e5d426f4cba0f7ce75cd888a47db6
RenePeuser - Rene Peuser <rene.peuser@hotmail.de>
2016-06-11 22:05:43 +0200

    remove static extensions and reference my omega extensions lib

--- CustomSort/CustomSort/Behaviors/DataGridCompositeCollectionCustomSortBehavior.cs
+++ CustomSort/CustomSort/Behaviors/DataGridCompositeCollectionCustomSortBehavior.cs
@@ -4,7 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Interactivity;
 using CustomSort.Base;
-using CustomSort.Extensions;
+using WpfExtensions;
 
 namespace CustomSort.Behaviors
 {
